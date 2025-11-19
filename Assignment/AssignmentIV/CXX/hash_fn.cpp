8a28db5bb8bb0fb4850e8d7e66ab8929b0355055
waga6 <ohai333666999@gmail.com>
2025-11-19 21:25:36 +0800

    Update hash_fn.cpp

--- Assignment/AssignmentIV/CXX/hash_fn.cpp
+++ Assignment/AssignmentIV/CXX/hash_fn.cpp
@@ -13,14 +13,29 @@
    Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
  */
 #include "hash_fn.hpp"
+#include <cmath>// for std::floor and std::pow
 
 int myHashInt(int key, int m) {
     // TODO: replace with your own design
+    double A = 0.6180339887;
+    /*錯誤寫法：因 % 是整數取餘數運算符，不能用在double/float
+    double h = floor(m*((key*A)%1));
+    key = h;
+    */
+    double temp = key * A - floor(key * A);
+    double temp2 = floor(m * temp);
+    key = static_cast<int>(temp2);
     return key % m;  // basic division method
 }
 
 int myHashString(const std::string& str, int m) {
     unsigned long hash = 0;
     // TODO: replace with your own design
+    int count = 0, temp = 0, p = 31;
+    for (int i = 0; i < str.size(); i++) {
+        temp = str[i] - '0';
+        count += temp * pow(p, i);
+    }
+    hash = static_cast<unsigned long>(count);
     return static_cast<int>(hash % m);  // basic division method
 }
