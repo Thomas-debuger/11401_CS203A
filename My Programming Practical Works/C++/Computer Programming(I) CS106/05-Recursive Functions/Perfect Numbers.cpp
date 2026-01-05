d5f0921f6b98ea3778335ca7a23fb2f243a5494c
Thomas-debuger <thomaswei988@gmail.com>
2026-01-05 22:18:41 +0800

    Create Perfect Numbers.cpp

---
+++ My Programming Practical Works/C++/Computer Programming(I) CS106/05-Recursive Functions/Perfect Numbers.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace::std;
+
+// find the sum of factors of n in the range [ start .. n - 1 ]
+int sumFactors(int n, int start);
+
+int main()
+{
+	int numCases;
+	cin >> numCases;
+	for (int i = 1; i <= numCases; i++)
+	{
+		int n;
+		cin >> n;
+
+		// n is equal to the sum of factors of n in the range [ 1 .. n - 1 ]
+		if (n == sumFactors(n, 1))
+			cout << "perfect number" << endl;
+		else
+			cout << "non-perfect number" << endl;
+	}
+}
+
+int sumFactors(int n, int start)
+{
+	if (n == start) {
+		return 0;
+	}
+	else if (n % start == 0) {
+		return start + sumFactors(n, start + 1);
+	}
+	else if (n % start != 0) {
+		return sumFactors(n, start + 1);
+	}
+}
